f4e6f1abf11165140f11c54811d54ab04a62bc8a
Fabrice <fabrice.normandin@gmail.com>
2018-01-29 16:16:05 -0500

    Fixed the Startup for Floating-Point Instructions
    Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

--- Lab1/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
+++ Lab1/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
@@ -185,12 +185,17 @@ __Vectors_Size  EQU  __Vectors_End - __Vectors
 Reset_Handler    PROC
                  EXPORT  Reset_Handler             [WEAK]
         IMPORT  SystemInit
+		
         IMPORT  __main
-
-                 LDR     R0, =SystemInit
+				 LDR.W R0, =0xE000ED88
+				 LDR R1, [R0]
+				 ORR R1, R1, #(0xF << 20)
+				 STR R1, [R0]
+				 DSB
+				 ISB
+				 LDR     R0, =__main
                  BLX     R0
-                 LDR     R0, =__main
-                 BX      R0
+                 ;BX      R0
                  ENDP
 
 ; Dummy Exception Handlers (infinite loops which can be modified)
