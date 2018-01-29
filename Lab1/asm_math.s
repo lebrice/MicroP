3d63e6388fab130304a634871b9e061e03c2537e
Fabrice <fabrice.normandin@gmail.com>
2018-01-29 17:47:25 -0500

    Started writing Assembly program
    Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

--- Lab1/asm_math.s
+++ Lab1/asm_math.s
@@ -6,17 +6,29 @@
 ; -------
 ; R0: testValues pointer
 ; R1: size
-; R3: asm_output pointer
-
-
-
+; R2: asm_output pointer
 
 asm_math
+	; set initial value of RMS, max, min, max_index and min_index.
+	VLDR.f32 S0, =0  ; initial RMS value is 0.
+	VLDR S1, [R0] ; Load in the first value as the initial max.
+	VMOV.f32 S2, S1 ; Set the first value as the initial min. (take it from S1 which we just loaded.)
+	MOV R3, #0 ; initial max index is 0.
+	MOV R4, #0 ; initial min index is 0.
+	
+	MOV R5, #0 ; R5 is the counter of how many loop iterations we've done.
 
-	MOV		R6, R0
-	MOV		R7, R1
-
-
+loop
+	VLDM R0, { S5 } ; Read (potentially multiple) floating point values, starting at address in R0.
+	; S5: the current latest value.
+	
+	VMLA.f32 S0, S5, S5 	; S0 = S0 + S5 * S5 (RMS calculation)
+	
+	ADD R5, R5, #1 ; increment the counter
+	CMP R1, R5
+	BNE loop
+	
+	
 
 	BX		LR
 	
