8f1756ed250e476025653e4eef6e2f796b2df5aa
Harley Wiltzer <harleyw@hotmail.com>
2018-04-01 15:36:55 -0400

    Creates lab5 branch for accelerometer

---
+++ Lab5/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Lab5' 
+ * Target:  'Lab5' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
