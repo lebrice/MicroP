f3ceb2423346278aeba5b0444e2d7543cc3237b3
Marcel Morin <morin.marcel@mac.com>
2018-04-11 15:30:05 -0400

    added nucleo scratch project

---
+++ project/nucleo_scratch/MDK-ARM/RTE/_nucleo_scratch/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'nucleo_scratch' 
+ * Target:  'nucleo_scratch' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
