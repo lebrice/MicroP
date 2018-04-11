1b07cdadca735a5a6df8b92a06f9536da7ac3e0e
Marcel Morin <morin.marcel@mac.com>
2018-04-10 22:26:34 -0400

    added GPIO pins for nucleo UART 'handshake'

--- project/discovery_scratch/Src/gpio.c
+++ project/discovery_scratch/Src/gpio.c
@@ -79,10 +79,13 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOD, LED3_Pin|LED4_Pin|LD5_Pin|LD6_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOD, DATA_INTERRUPT_Pin|IS_MIC_DATA_Pin|LED3_Pin|LED4_Pin 
+                          |LD5_Pin|LD6_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PDPin PDPin PDPin PDPin */
-  GPIO_InitStruct.Pin = LED3_Pin|LED4_Pin|LD5_Pin|LD6_Pin;
+  /*Configure GPIO pins : PDPin PDPin PDPin PDPin 
+                           PDPin PDPin */
+  GPIO_InitStruct.Pin = DATA_INTERRUPT_Pin|IS_MIC_DATA_Pin|LED3_Pin|LED4_Pin 
+                          |LD5_Pin|LD6_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
